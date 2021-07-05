9dd28e0f47235d8aa375444c7fe1cbcd862d4cd3
Anup <apgandhamwar@gmail.com>
2021-07-05 23:51:58 +0530

    Routing Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { RouterModule, Routes,Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class AppComponent {
   title = 'DemoApp';
   response = "Test";
-  constructor(private http: HttpClient) 
-  { 
+  constructor(private http: HttpClient, private _router: Router)
+  {
+    this._router.navigateByUrl('/students');
+
  //   this.http.get('http://demoapi-service:80/Demo', {responseType: 'text'}).subscribe((response: any) => {
  //     console.log(response);
 	//  this.response = response;		
